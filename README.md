881c0460d410f7dba3b25b9924796a5e86e096ec
vwmaus <vwmaus1@gmail.com>
2023-08-05 23:33:27 +0200

    Integrates Liang's data. Corrects calculation of geometries difference.

--- README.md
+++ README.md
@@ -4,16 +4,23 @@ This repository contains the R script used to produce the mining data gap map fo
 
 ## Usage
 
-The script requires two datasets. The first consists of polygons that cover global mine areas, derived from satellite images acquired circa 2019 (Maus et al., 2022). This dataset will be automatically downloaded by the `main.R` script.
+The script requires three datasets. The first one includes polygons representing global mining areas. These polygons were derived from satellite images acquired in 2019 (Maus et al., 2020, 2022). The second dataset also includes information on global mining areas. It was derived from satellite images acquared over multiple years (Tang & Werner, 2023). The `main.R` script will automatically download and merge these datasets to achieve a more comprehensive coverage of global mining areas.
 
-The second dataset consists of 6,201 coordinates of mining properties extracted from the SNL Metals & Mining Dataset by S&P Global Marketplace. This subset of mines includes all mines that have reported production at any time between 2000 and 2019. The script expects this data in the file `./data/snl2020.gpkg`. If the file has a different name, you can modify line 41 in `main.R`. This dataset is not included with the repository, as we do not have the rights to share it.
+The third dataset contains the coordinates of 6,201 mining properties, extracted from the SNL Metals & Mining Dataset by S&P Global Marketplace. This dataset includes all mines that have reported production at any time between 2000 and 2019. The script expects this data in the file `./data/snl2020.gpkg`. If the data file is named differently, you can adjust this at line 30 in the `main.R` script. Please note that this third dataset is not included in the repository, as we do not have the rights to distribute it.
 
 ## Output
 
-The script will create a global grid of 50 km x 50 km cells and check which cells contain a production data gap, i.e., mining areas identified in the satellite images but with no information on production in the SNL database. The final map, highlighting gap cells in red, will be written to the PDF file `./data/gap_map_50km_grid.pdf`.
+The script conducts a calculation to determine the global share of mining areas that fall outside a 10 km buffer around the coordinates of mining properties. Mining areas situated outside this buffer cannot be readily associated with any of the 6,201 identified properties. The results are then aggregated to a grid of 50 km x 50 km cells. The map below shows the maximum distance of mining areas within each grid cell to the nearest property with production information. Only cells with at least one mining polygon farther than 10 km from the nearest property are displaed in this map.
+
+![](./gap_dist_grid.png)
+
+The script also generates a binary gap map. Cells with mining area but missing data on production are marked as `1`, while all others are labeled as `0`. This map is written in GeoTIFF format to `./data/gap_grid.tif` and also saved as a PDF file at `./data/gap_grid_map.pdf`.
+
 
 ## References
 
 Maus, V., Giljum, S., da Silva, D.M., Gutschlhofer, J., da Rosa, R.P., Luckeneder, S., Gass, S.L., Lieber, M., and McCallum, I., 2022. An Update on Global Mining Land Use. Scientific Data, 9(1), pp.1-11.
+
+Maus, V., Giljum, S., Gutschlhofer, J., da Silva, D.M., Probst, M., Gass, S.L., Luckeneder, S., Lieber, M., and McCallum, I., 2020. A global-scale data set of mining areas. Scientific Data, 7(289), pp.1-13.
 
 Tang, L., and Werner, T.T., 2023. Global Mining Footprint Mapped from High-Resolution Satellite Imagery. Communications Earth & Environment, 4(1), p.134.
--- main.R
+++ main.R
@@ -6,10 +6,10 @@ library(units)
 library(ggthemes)
 library(ggplot2)
 library(plyr)
-
+source("s2_union_split_agg.R")
 dir.create("./data", showWarnings = FALSE)
 
-# Define grid parameters - 5 arcmin (~10km x 10km)
+# Define grid parameters - (~50km x 50km)
 crs <- st_crs(4326)
 grid_template <- st_as_stars(st_bbox(c(xmin = -180, xmax = 180, ymin = -90, ymax = 90), crs = crs), nx = 720, ny = 360, values = 0)
 grid_template$id <- 1:prod(dim(grid_template))
@@ -25,35 +25,144 @@ bbox_robinson <- st_bbox(c(xmin = -145, xmax = 180, ymin = -55, ymax = 80), crs 
 ylim_robinson <- c(st_bbox(bbox_robinson)["ymin"], st_bbox(bbox_robinson)["ymax"])
 xlim_robinson <- c(st_bbox(bbox_robinson)["xmin"], st_bbox(bbox_robinson)["xmax"])
 
-# Download mining land use data from Maus et al. 2022
-# This dataset represents the mining land use circa 2019
-if(!file.exists("./data/global_mining_polygons_v2.gpkg")){
-  download.file("https://download.pangaea.de/dataset/942325/files/global_mining_polygons_v2.gpkg", destfile = "./data/global_mining_polygons_v2.gpkg")
-}
-mining_land_use <- st_read("./data/global_mining_polygons_v2.gpkg", quiet = TRUE) |> 
-  dplyr::select(geometry = "geom") |> 
-  st_cast("POLYGON") |> 
-  dplyr::filter(st_geometry_type(geometry) %in% c("POLYGON"), !st_is_empty(geometry)) |>
-  st_transform(crs) |>
-  dplyr::rename(geom = geometry) |>
-  mutate(area = set_units(st_area(geom), km^2))
-
 # Read mining properties from reporting production at any time between 2000 and 2017
 # This data was extracted from the S&P SNL database as of 2020
 mining_properties <- st_read("./data/snl2020.gpkg", quiet = TRUE) |>
   st_transform(crs)
 
-# Calculate the distance from each mining land use polygon to nearest mine property
-nearest_property <- mining_land_use |>
-  st_nearest_feature(mining_properties)
+if(!file.exists("./data/mining_properties_buffer.gpkg")){
 
-mining_land_use$dist <- mining_land_use |>
-  st_distance(mining_properties[nearest_property,], by_element = TRUE) |>
-  set_units(km)
+# Create a buffer around the mining properties
+mining_properties_buffer <- st_buffer(mining_properties, set_units(10, km)) |>
+  st_as_s2(mining_properties_buffer) |>
+  s2_union_split_agg(options = s2_options(model = "closed")) |>
+  st_as_sf() |>
+  select(geom = "geometry") |>
+  filter(st_geometry_type(geom) %in% c("POLYGON", "MULTIPOLYGON")) |>
+  st_cast("POLYGON") |>
+  st_make_valid() |>
+  filter(st_is_valid(geom)) |>
+  st_transform(crs)
 
-mining_properties_buffer <- st_buffer(mining_properties, set_units(10, km))
+  st_write(mining_properties_buffer, "./data/mining_properties_buffer.gpkg", driver = "GPKG")
 
-### Spatial relation checks
+}
+
+mining_properties_buffer <- st_read("./data/mining_properties_buffer.gpkg", quiet = TRUE)
+
+# Merge global mining land datasets if not processed yet
+if(!file.exists("./data/global_mining_land_use.gpkg")){
+  # Download mining land use data from Maus et al. 2022 (doi: 10.1594/PANGAEA.942325)
+  # This dataset was derived from satellite images acquared in 2019
+  if(!file.exists("./data/maus2022.gpkg")){
+    download.file("https://download.pangaea.de/dataset/942325/files/global_mining_polygons_v2.gpkg", destfile = "./data/maus2022.gpkg")
+  }
+  maus2022 <- st_read("./data/maus2022.gpkg", quiet = TRUE) |>
+    select(geom) |>
+    st_cast("POLYGON") |>
+    dplyr::filter(st_geometry_type(geom) %in% c("POLYGON"), !st_is_empty(geom)) |>
+    st_transform(crs)
+
+  # Download mining land use data from Tang & Werner 2023 (doi: 10.5281/zenodo.7894216)
+  # This dataset represents the mining land use derived from images acquared in various years
+  options(timeout = 600)
+  download.file(url = "https://zenodo.org/record/7894216/files/74548_projected%20polygons.dbf?download=1",
+                destfile = "./data/tang2023.dbf", mode = "wb")
+  download.file(url = "https://zenodo.org/record/7894216/files/74548_projected%20polygons.prj?download=1",
+                destfile = "./data/tang2023.prj", mode = "wb")
+  download.file(url = "https://zenodo.org/record/7894216/files/74548_projected%20polygons.shp?download=1",
+                destfile = "./data/tang2023.shp", mode = "wb")
+  download.file(url = "https://zenodo.org/record/7894216/files/74548_projected%20polygons.shx?download=1",
+                destfile = "./data/tang2023.shx", mode = "wb")
+
+  tang2023 <- st_read("./data/tang2023.shp", quiet = TRUE) |>
+    dplyr::select(geom = "geometry") |>
+    st_cast("POLYGON") |>
+    st_simplify() |>
+    st_make_valid() |>
+    filter(st_is_valid(geom)) |>
+    dplyr::filter(st_geometry_type(geom) %in% c("POLYGON"), !st_is_empty(geom)) |>
+    st_transform(crs)
+
+  # Merge mining land use datasets and and remove overlaps
+  mining_land_use <- bind_rows(maus2022, tang2023) |>
+    filter(s2_is_valid(geom)) |>
+    st_as_s2() |>
+    s2_union_split_agg(options = s2_options(model = "closed")) |>
+    st_as_sf() |>
+    select(geom = "geometry") |>
+    st_cast("POLYGON") |>
+    st_make_valid() |>
+    filter(st_is_valid(geom)) |>
+    mutate(area = set_units(st_area(geom), km^2))
+
+  # Save mining land use data
+  st_write(mining_land_use, "./data/global_mining_land_use.gpkg", delete_layer = TRUE)
+
+  # remove variables clear cache
+  rm(mining_land_use, tang2023, maus2022)
+  gc(reset = TRUE, full = TRUE)
+
+}
+
+mining_land_use <- st_read("./data/global_mining_land_use.gpkg", quiet = TRUE)
+
+# Calculate intersetion between mining land use and mining properties buffers
+if(!file.exists("./data/mining_land_use_diff_10km.gpkg")){
+
+  buffers_intersects <- st_intersects(mining_land_use, mining_properties_buffer)
+
+  mining_land_use_diff_10km <- llply(seq_along(buffers_intersects), .progress = "text", function(i){
+    
+    bf <- st_geometry(mining_properties_buffer[buffers_intersects[[i]], ]) |>
+      st_union()
+
+    if(length(bf) < 1){
+      return(mining_land_use[i,'geom'])
+    }
+    mining_land_use[i, ] |>
+      select(geom) |>
+      st_difference(bf)
+  }) |>
+    bind_rows()
+
+  st_write(mining_land_use_diff_10km, "./data/mining_land_use_diff_10km.gpkg", delete_layer = TRUE)
+
+  # remove variables clear cache
+  rm(mining_land_use_diff_10km, buffers_intersects)
+  gc(reset = TRUE, full = TRUE)
+
+}
+
+mining_land_use_diff_10km <- st_read("./data/mining_land_use_diff_10km.gpkg", quiet = TRUE) |>
+    mutate(area = set_units(st_area(geom), km^2))
+
+mining_land_use_diff_10km$area |> sum() / mining_land_use$area |> sum()
+
+# Create mining land use grid
+gap_grid <- mining_land_use_diff_10km |>
+  dplyr::transmute(dataset = 1, geom = st_centroid(geom)) |>
+  st_rasterize(template = grid_template, file = "./data/gap_grid.tif", proxy = FALSE, options = "ALL_TOUCHED=TRUE") |>
+  st_as_sf() |>
+  filter(dataset == 1)
+
+# Plot gap map with 50km grid
+gp <- ggplot(data = world_map) +
+  geom_sf(fill = "#D3D3D3", colour = NA) +
+  xlab("Longitude") + ylab("Latitude") +
+  theme_map() +
+  geom_sf(data = gap_grid, fill = "#cc3e5b", colour = NA) +
+  theme(
+    legend.position = 'none',
+    plot.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = "pt")) + 
+  coord_sf(xlim = xlim_robinson, ylim = ylim_robinson, crs = st_crs("+proj=robin"))
+
+ggsave(gp, filename = "./data/gap_grid_map.pdf", width = 12.1, height = 6.5, dpi = 300)
+ggsave(gp, filename = "./data/gap_grid_map.png", width = 12.1, height = 6.5, dpi = 300)
+
+
+
+### Other spatial relation checks
 
 # Calculate mining land use intersecting 10km buffer from mining properties
 mining_land_use_intersects_10km <- st_filter(mining_land_use, mining_properties_buffer, .predicate = st_intersects)
@@ -72,78 +181,49 @@ mining_land_use_centroid_within_10km <- mining_land_use |>
 sum(mining_land_use_centroid_within_10km$area) / sum(mining_land_use$area)
 nrow(mining_land_use_centroid_within_10km) / nrow(mining_land_use)
 
-# Calculate intersetion between mining land use and mining properties buffers
-buffers_intersects <- st_intersects(mining_land_use, mining_properties_buffer)
-
-mining_land_use_diff_10km <- llply(seq_along(buffers_intersects), .progress = "text", function(i){
-  bf <- st_geometry(mining_properties_buffer[buffers_intersects[[i]], ])
-  if(length(bf) < 1){
-    return(mining_land_use[i,'geom'])
-  }
-  mining_land_use[i, ] |>
-    select(geom) |>
-    st_difference(bf)
-}) |>
-  bind_rows() |>
-  mutate(area = set_units(st_area(geom), km^2))
-
-mining_land_use_diff_10km$area |> sum() / mining_land_use$area |> sum()
-
-
-# Create mining land use grid
-mining_land_use_grid <- mining_land_use |>
-  dplyr::transmute(dataset = 1) |>
-  st_rasterize(template = grid_template, proxy = FALSE, options = "ALL_TOUCHED=TRUE")
-
-# Create mining properties grid
-mining_properties_grid <- mining_properties |>
-  dplyr::transmute(dataset = 1) |>
-  st_rasterize(template = grid_template, proxy = FALSE, options = "ALL_TOUCHED=TRUE")
-
-# Calculate grid cells statistics
-matches_cells <- mining_land_use_grid + ( 2 * mining_properties_grid)
-matches_cells <- matches_cells * matches_cells / matches_cells
-cell_freq <- table(matches_cells)
-round(cell_freq / sum(cell_freq) * 100, 2)
-
-gap_cells <- st_as_sf(matches_cells) |>
-  filter(dataset == 1)
-
-# Plot gap map with 50km grid
-gp <- ggplot(data = world_map) +
-  geom_sf(fill = "#D3D3D3", colour = NA) +
-  xlab("Longitude") + ylab("Latitude") +
-  theme_map() +
-  geom_sf(data = gap_cells, fill = "#cc3e5b", colour = NA) +
-  theme(
-    legend.position = 'none',
-    plot.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = "pt")) + 
-  coord_sf(xlim = xlim_robinson, ylim = ylim_robinson, crs = st_crs("+proj=robin"))
-
-ggsave(gp, filename = "./data/gap_map_50km_grid.pdf", width = 12.1, height = 6.5, dpi = 300)
-ggsave(gp, filename = "./data/gap_map_50km_grid.png", width = 12.1, height = 6.5, dpi = 300)
 
 # Gap distance
+# Calculate the distance from each mining land use polygon to nearest mine property
+nearest_property <- mining_land_use |>
+  st_nearest_feature(mining_properties)
+
+mining_land_use$dist <- mining_land_use |>
+  st_distance(mining_properties[nearest_property,], by_element = TRUE) |>
+  set_units(km)
+
 dist_grid <- select(st_as_sf(grid_template), id)
 
 nearest_property_dist_grid <- mining_land_use |>
   st_join(dist_grid, st_intersects) |>
   st_drop_geometry() |>
   group_by(id) |>
-  summarise(dist = max(dist), area = mean(area))
+  dplyr::summarise(dist = max(dist), area = mean(area)) 
 
-dist_grid <- dist_grid |>
+dist_grid_map <- dist_grid |>
   left_join(nearest_property_dist_grid) |>
-  filter(!is.na(area))
+  filter(!is.na(area)) |>
+  filter(dist > set_units(10, km))
 
 gp <- ggplot(data = world_map) +
   geom_sf(fill = "#D3D3D3", colour = NA) +
   xlab("Longitude") + ylab("Latitude") +
   theme_map() +
-  geom_sf(data = dist_grid, aes(fill = log(as.numeric(dist))), colour = NA) +
+  geom_sf(data = dist_grid_map, aes(fill = as.numeric(dist)), colour = NA) +
   theme(
-    legend.position = 'none',
-    plot.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = "pt")) + 
+    legend.position = "bottom",
+    legend.direction = "horizontal",
+    legend.justification = "center",
+    legend.box.spacing = unit(0.0, "cm"),
+    legend.key.size = unit(0.3, "cm"),
+    legend.key.width = unit(35, "mm"),
+    plot.margin = margin(t = -1, r = -1, b = 0, l = -1, unit = "cm")
+  ) +
+  labs(fill = bquote(Distance~to~nearest~property~(km))) +
+  viridis::scale_fill_viridis(option = "turbo", 
+    labels = seq(10, 1300, 290), 
+    breaks = seq(10, 1300, 290), 
+    limits = c(10, max(dist_grid_map$dist)),
+    begin = 0.15, end = 1, direction = 1, discrete = FALSE) +
   coord_sf(xlim = xlim_robinson, ylim = ylim_robinson, crs = st_crs("+proj=robin"))
 
-ggsave(gp, filename = "./data/gap_map_50km_dist_grid.pdf", width = 12.1, height = 6.5, dpi = 300)
+ggsave(gp, filename = "./gap_dist_grid.png", width = 12.1, height = 6.5, dpi = 300)
